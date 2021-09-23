7bfdb02b60dd15167f518c072dedebf192d28001
Lana Novikova <svetlnovikova@gmail.com>
2021-09-23 21:57:47 +0200

    first commit

---
+++ weather.py
@@ -0,0 +1,30 @@
+import requests 
+import settings
+
+def weather_by_city(city_name):
+    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    params = {
+        "key": settings.API_KEY,
+        "q": city_name,
+        "format": "json",
+        "num_of_days": 1,
+        "lang": "ru"
+    }
+    try:
+        result = requests.get(weather_url, params = params)
+        result.raise_for_status()
+        weather = result.json()
+        if 'data' in weather:
+            if 'current_condition' in weather['data']: 
+                try:
+                    return weather['data']['current_condition'][0]
+                    #weather['data']['request'][1]
+                except(IndexError,TypeError):
+                    return False 
+    except(requests.RequestException, ValueError):
+        print('Ошибка сети')
+        return False
+    return False
+
+if __name__ == "__main__":
+    print(weather_by_city("Moscow,Russia"))
